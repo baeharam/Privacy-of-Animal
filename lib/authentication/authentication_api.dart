7bdc411cfe1d54b9ae32812ca51c9d9ea09c9579
bae haram <gkfkagkfka12@naver.com>
2019-01-23 14:37:00 +0900

    Add error handling
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/authentication/authentication_api.dart
+++ lib/authentication/authentication_api.dart
@@ -1,24 +1,39 @@
 import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 
 class AuthenticationAPI {
 
   static final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  static const String SUCCESS = 'SUCCESS';
+  static const String FAILURE = 'FAILURE';
+
   // 회원가입
-  Future<FirebaseUser> signUpWithFirebase(String email, String password) async {
-    return await _auth.createUserWithEmailAndPassword(
+  Future<String> signUpWithFirebase(String email, String password) async {
+    await _auth.createUserWithEmailAndPassword(
       email: email,
       password: password
-    );
+    ).catchError((exception){
+      if(exception is PlatformException){
+        return FAILURE;
+      }
+    });
+    return SUCCESS;
   }
 
   // 로그인
-  Future<FirebaseUser> loginWithFirebase(String email, String password) async {
-    return await _auth.signInWithEmailAndPassword(
+  Future<String> loginWithFirebase(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(
       email: email,
       password: password
-    );
+    ).catchError((exception){
+      if(exception is PlatformException){
+        return FAILURE;
+      }
+    });
+    return SUCCESS;
   }
 }
+
--- lib/authentication/authentication_bloc.dart
+++ lib/authentication/authentication_bloc.dart
@@ -1,13 +1,19 @@
-
-import 'package:privacy_of_animal/authentication/authentication_event.dart';
-import 'package:privacy_of_animal/authentication/authentication_state.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:privacy_of_animal/authentication/authentication.dart';
 import 'package:privacy_of_animal/bloc_helpers/bloc_event_state.dart';
 
 class AuthenticaionBloc extends BlocEventStateBase<AuthenticationEvent,AuthenticationState> {
+
+  static final AuthenticationAPI _api = AuthenticationAPI();
+
   @override
   Stream<AuthenticationState> eventHandler(AuthenticationEvent event, AuthenticationState currentState) async*{
     
+    if(event is AuthenticationEventLogin){
+      FirebaseUser loginResult = 
+        await _api.loginWithFirebase(event.email, event.password);
+    }
+
     return null;
   }
-
 }
