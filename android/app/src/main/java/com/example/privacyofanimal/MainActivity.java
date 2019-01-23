e4c0b2209a2c4f635549ef262da37cd9e386538c
bae haram <gkfkagkfka12@naver.com>
2019-01-23 15:42:11 +0900

    Add splash image for android
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- android/app/src/main/java/com/example/privacyofanimal/MainActivity.java
+++ android/app/src/main/java/com/example/privacyofanimal/MainActivity.java
@@ -8,6 +8,15 @@ public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getWindow().setStatusBarColor(0x00000000);
     GeneratedPluginRegistrant.registerWith(this);
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
