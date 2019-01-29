3ad88c431c266cbb9d68b0b8be18a2b6ff699950
bae haram <gkfkagkfka12@naver.com>
2019-01-29 21:18:57 +0900

    For pull request
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/utils/image_precache.dart
+++ lib/utils/image_precache.dart
@@ -1,8 +1,13 @@
+import 'dart:isolate';
+
 import 'package:flutter/widgets.dart';
 import 'package:privacy_of_animal/resources/constants.dart';
 
+Isolate isolate;
 
-Future<void> imagePrecache(BuildContext context) async{
+void imagePrecache(BuildContext context) async{
+  ReceivePort receivePort = ReceivePort();
+  isolate = await Isolate.spawn(entryPoint, message)
   for(int i=0; i<tags.length; i++){
     precacheImage(AssetImage(tags[i].image), context);
   }
