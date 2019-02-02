58b4e71c097909af2a04861d042f6aafced00af8
bae haram <gkfkagkfka12@naver.com>
2019-02-02 17:46:11 +0900

    Add fake profile model
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

---
+++ lib/models/fake_profile_model.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+class FakeProfileModel {
+  final String image;
+  final String animalName;
+
+  FakeProfileModel({
+    @required this.image,
+    @required this.animalName
+  });
+}
---
+++ lib/screens/main/analyze_result_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AnalyzeResultScreen extends StatefulWidget {
+  @override
+  _AnalyzeResultScreenState createState() => _AnalyzeResultScreenState();
+}
+
+class _AnalyzeResultScreenState extends State<AnalyzeResultScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
