7c7cb33187b6f01f75481cca11443161bc2a915e
bae haram <gkfkagkfka12@naver.com>
2019-02-02 11:45:35 +0900

    Fix format
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/screens/main/tag_photo_screen.dart
+++ lib/screens/main/tag_photo_screen.dart
@@ -16,7 +16,6 @@ class _PhotoScreenState extends State<PhotoScreen> {
   
   @override
   Widget build(BuildContext context){
-    double height = MediaQuery.of(context).size.height/ 3.0;
     return Scaffold(
       appBar: AppBar(
         title: Text("사진"),
@@ -27,73 +26,63 @@ class _PhotoScreenState extends State<PhotoScreen> {
           child: SafeArea(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-  
-            BlocBuilder(
-              bloc: _photoBloc,
-              builder: (context, PhotoState state){
-                if(state.takedPhoto) {
-                  return Container(
-                    height: height/1,
-                    // width: ScreenUtil.width/1.4,
-                    child: FittedBox(
-                      // child: Text(state.path),
-                      fit:BoxFit.contain,
-                      child: Image.file(
-                        File(state.path),
-                        // fit:BoxFit.fitHeight,
-                        // height: height,
-                        )
-                      )
-                    );
-                  
-                  }
-                else
-                  {
-                    _photoBloc.emitEvent(PhotoEventTaking());
-                    return Container(
-                      child: SafeArea(
-                        child: new CircularProgressIndicator(),
-                      ),
-                      height: 30.0,
-                      width: 30.0,
-                    );
-                  }
-                },
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height/18,
-              ),
-              RaisedButton(
-                child: Text(
-                  "사진 다시 찍기",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.w600
+              children: <Widget>[
+                BlocBuilder(
+                  bloc: _photoBloc,
+                  builder: (context, PhotoState state){
+                    if(state.takedPhoto) {
+                      return Container(
+                        height: ScreenUtil.height,
+                        child: FittedBox(
+                          fit:BoxFit.contain,
+                          child: Image.file(
+                            File(state.path),
+                            )
+                          )
+                        );
+                      
+                      }
+                    else
+                      {
+                        _photoBloc.emitEvent(PhotoEventTaking());
+                        return Container(
+                          child: SafeArea(
+                            child: new CircularProgressIndicator(),
+                          ),
+                          height: 30.0,
+                          width: 30.0,
+                        );
+                      }
+                    },
                   ),
-                  ),
-                  onPressed: () => _photoBloc.emitEvent(PhotoEventTaking()),
-                  color: primaryBeige,
-              ),
-            RaisedButton(
-              child: Text(
-                "가상프로필 생성 ",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.w600
+                SizedBox(height: ScreenUtil.height/18),
+                RaisedButton(
+                  child: Text(
+                    "사진 다시 찍기",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600
+                    ),
+                    ),
+                    onPressed: () => _photoBloc.emitEvent(PhotoEventTaking()),
+                    color: primaryBeige,
                 ),
-              ),
-              onPressed: () async{
-                _photoBloc.emitEvent(PhotoEventGotoAnalysis());
-                // TODO:goto next screen
-              },
-              color: primaryBeige,
-            )
+                RaisedButton(
+                  child: Text(
+                    "가상프로필 생성 ",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600
+                    ),
+                  ),
+                  onPressed: () => _photoBloc.emitEvent(PhotoEventGotoAnalysis()),
+                  color: primaryBeige,
+                )
 
-          ],
-        ),
+              ],
+            ),
           ),
       ),
     );
