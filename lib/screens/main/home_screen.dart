262916e571d0f262788b4d7cbbaeb31ffa487553
bae haram <gkfkagkfka12@naver.com>
2019-02-04 16:34:00 +0900

    Set home screen
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:privacy_of_animal/resources/constants.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,25 +12,39 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Container(),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
+        currentIndex: 3,
         items: [
           BottomNavigationBarItem(
             title: Text(''),
-            icon: Image.asset('assets/images/components/match.png')
+            icon: ImageIcon(
+              AssetImage('assets/images/components/match.png'),
+              size: ScreenUtil.width/4,
+            )
           ),
           BottomNavigationBarItem(
             title: Text(''),
-            icon: Image.asset('assets/images/components/chat.png')
+            icon: ImageIcon(
+              AssetImage('assets/images/components/chat.png'),
+              size: ScreenUtil.width/4,
+            )
           ),
           BottomNavigationBarItem(
             title: Text(''),
-            icon: Image.asset('assets/images/components/friend.png')
+            icon: ImageIcon(
+              AssetImage('assets/images/components/friend.png'),
+              size: ScreenUtil.width/4,
+            )
           ),
           BottomNavigationBarItem(
             title: Text(''),
-            icon: Image.asset('assets/images/components/profile.png')
+            icon: ImageIcon(
+              AssetImage('assets/images/components/profile.png'),
+              size: ScreenUtil.width/4,
+            )
           )
         ],
+        fixedColor: Colors.red,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
