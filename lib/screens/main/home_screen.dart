df7c72edb2a83a9acc6f0262947979af56686c8b
bae haram <gkfkagkfka12@naver.com>
2019-02-04 15:53:29 +0900

    Test bottom bar
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/screens/main/home_screen.dart
+++ lib/screens/main/home_screen.dart
@@ -8,8 +8,25 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/components/match.png')
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/components/chat.png')
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/components/friend.png')
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/components/profile.png')
+          )
+        ],
+      ),
     );
   }
 }
