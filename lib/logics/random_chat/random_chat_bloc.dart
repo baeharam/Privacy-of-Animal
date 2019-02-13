89f54caefbe8f31d9c9f9d16e83d8113ae988da6
bae haram <gkfkagkfka12@naver.com>
2019-02-13 11:26:44 +0900

    Fix iOS issue of tag_select_screen
    Signed-off-by: bae haram <gkfkagkfka12@naver.com>

--- lib/logics/random_chat/random_chat_bloc.dart
+++ lib/logics/random_chat/random_chat_bloc.dart
@@ -1,7 +1,5 @@
 import 'package:privacy_of_animal/bloc_helpers/bloc_event_state.dart';
-import 'package:privacy_of_animal/logics/current_user.dart';
 import 'package:privacy_of_animal/logics/random_chat/random_chat.dart';
-import 'package:privacy_of_animal/utils/service_locator.dart';
 
 class RandomChatBloc extends BlocEventStateBase<RandomChatEvent,RandomChatState>
 {
